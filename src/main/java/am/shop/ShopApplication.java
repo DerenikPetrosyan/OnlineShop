1ae5e984fe08539c8b20a09dead49ee37b725615
DerenikPetrosyan <deren_petrosyan@mail.ru>
2022-08-06 22:29:52 +0400

    add web page

--- src/main/java/am/shop/ShopApplication.java
+++ src/main/java/am/shop/ShopApplication.java
@@ -2,7 +2,6 @@ package am.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ShopApplication {
 
